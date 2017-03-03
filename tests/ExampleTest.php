8a927749cb68fc0f6d1b1cc9679724d77ecbfa56
itsDamien <its@damien.is>
2017-03-03 09:58:38 +0100

    Add Travis + Coveralls

---
+++ tests/ExampleTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace ItsDamien\Api\Tests;
+
+class ExampleTest extends \PHPUnit_Framework_TestCase
+{
+    public function testAssertIsTrue()
+    {
+        $this->assertTrue(true);
+    }
+}
